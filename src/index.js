03486d4155a96dbb9f4a3219caa6c2753e7883b2
Alex Ross <alxrg@me.com>
2022-04-02 14:12:40 -0500

    added projects folder and updated js files

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About/About'
 import SercivesList from './components/ServicesList/ServicesList';
+import ProjectsList from './components/ProjectsList/ProjectList';
 import Main from './components/Main/Main'
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import ServicesDetails from './components/ServicesDetails/ServicesDetails';
+import ProjectsDetails from './components/ProjectsDetails/ProjectsDetails';
 import Contact from './components/Contact/Contact'
 
 ReactDOM.render(
@@ -17,8 +19,10 @@ ReactDOM.render(
         
         <Route path="/" element={<Main />} />
         <Route path="services" element={<SercivesList />} />
+        <Route path="projects" element={<ProjectsList />} />
         <Route path="about" element={<About />} />
         <Route path="/details/:id" element={<ServicesDetails />} />
+        <Route path="/projects/:id" element={<ProjectsDetails />} />
         <Route path="/contact" element={<Contact />} />
 
       </Route>
